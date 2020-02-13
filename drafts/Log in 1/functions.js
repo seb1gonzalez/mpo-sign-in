a99fc8b873f0a3b5694355c7065e73dff1a10a62
Victor Vargas <vevargascor@miners.utep.edu>
2020-02-13 11:28:40 -0700

    Removed other unnecessary draft

--- drafts/Log in 1/functions.js
+++
@@ -1,9 +0,0 @@
-function togglePassVisibility() {
-    var x = document.getElementById("password");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  }
-
